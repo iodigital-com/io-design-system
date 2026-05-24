753a0c99b96a4d831dbb54b0e92f610f61d7566a
Jake Ortega <jakeortega-io@github.com>
2026-05-24 09:50:50 +0200

    fix(io-button-group): use IoButtonGroupSize instead of string in propagateSize cast

--- io-components/src/components/io-button-group/io-button-group.tsx
+++ io-components/src/components/io-button-group/io-button-group.tsx
@@ -165,7 +165,7 @@ export class IoButtonGroup {
     (slot as HTMLSlotElement)
       .assignedElements({ flatten: true })
       .filter(el => el.tagName === 'IO-BUTTON')
-      .forEach(btn => ((btn as Element & { size: string }).size = this.size));
+      .forEach(btn => ((btn as Element & { size: IoButtonGroupSize }).size = this.size));
   }
 
   private handleItemClick(index: number) {

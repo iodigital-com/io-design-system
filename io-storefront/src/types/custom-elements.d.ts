ff86acf5ab79337a7b4c8561b19a5ac88b233374
Jake Ortega <jakeortega-io@github.com>
2026-06-18 21:06:16 +0200

    fix(io-sheet): resolve lint unused-var and sync stencil assets

--- io-storefront/src/types/custom-elements.d.ts
+++ io-storefront/src/types/custom-elements.d.ts
@@ -43,10 +43,10 @@ declare module 'react' {
   'io-radio': React.DetailedHTMLProps<React.HTMLAttributes<HTMLIoRadioElement>, HTMLIoRadioElement> & Partial<StencilJSX.IoRadio>;
   'io-radio-group': React.DetailedHTMLProps<React.HTMLAttributes<HTMLIoRadioGroupElement>, HTMLIoRadioGroupElement> & Partial<StencilJSX.IoRadioGroup>;
   'io-scroller': React.DetailedHTMLProps<React.HTMLAttributes<HTMLIoScrollerElement>, HTMLIoScrollerElement> & Partial<StencilJSX.IoScroller>;
-  'io-sheet': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & { open?: boolean; heading?: string; dismissible?: boolean; onDismiss?: (e: CustomEvent<void>) => void };
   'io-segment': React.DetailedHTMLProps<React.HTMLAttributes<HTMLIoSegmentElement>, HTMLIoSegmentElement> & Partial<StencilJSX.IoSegment>;
   'io-segmented-control': React.DetailedHTMLProps<React.HTMLAttributes<HTMLIoSegmentedControlElement>, HTMLIoSegmentedControlElement> & Partial<StencilJSX.IoSegmentedControl>;
   'io-select': React.DetailedHTMLProps<React.HTMLAttributes<HTMLIoSelectElement>, HTMLIoSelectElement> & Partial<StencilJSX.IoSelect>;
+  'io-sheet': React.DetailedHTMLProps<React.HTMLAttributes<HTMLIoSheetElement>, HTMLIoSheetElement> & Partial<StencilJSX.IoSheet>;
   'io-spinner': React.DetailedHTMLProps<React.HTMLAttributes<HTMLIoSpinnerElement>, HTMLIoSpinnerElement> & Partial<StencilJSX.IoSpinner>;
   'io-step': React.DetailedHTMLProps<React.HTMLAttributes<HTMLIoStepElement>, HTMLIoStepElement> & Partial<StencilJSX.IoStep>;
   'io-stepper': React.DetailedHTMLProps<React.HTMLAttributes<HTMLIoStepperElement>, HTMLIoStepperElement> & Partial<StencilJSX.IoStepper>;
